e886118add4bbeb9c2b9a37c3273a27baa0fc624
linzc <linzicong@ppmoney.com>
2016-10-19 20:53:24 +0800

    dev 添加实例

--- src/com/thread/lockExp.java
+++ src/com/thread/lockExp.java
@@ -7,45 +7,45 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
 
 public class lockExp {
 
-	public static void createBusyThread(){
-		Thread thread =new Thread(new Runnable() {
-				@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				while(true);
-			}
-		},"testBusyThread");
-		thread.start();
-	}
-	
-	//œﬂ≥ÃÀ¯µ»¥˝—› æ
-	public static void createLockThread(final Object lock) {
-		Thread thread =new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				synchronized (lock) {
-					try {
-						lock.wait();
-					} catch (InterruptedException e) {
-						// TODO: handle exception
-						e.printStackTrace();
-					}
-				}
-			}
-		},"testLockThread");
-		thread.start();
-	}
-	
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-BufferedReader bReader=new BufferedReader(new InputStreamReader(System.in));
-bReader.readLine();
-createBusyThread();
-bReader.readLine();
-Object object= new Object();
-createLockThread(object);
-	}
+    public static void createBusyThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (true) ;
+            }
+        }, "testBusyThread");
+        thread.start();
+    }
+
+    //Á∫øÁ®ãÈîÅÁ≠âÂæÖÊºîÁ§∫
+    public static void createLockThread(final Object lock) {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                synchronized (lock) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        // TODO: handle exception
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "testLockThread");
+        thread.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        bReader.readLine();
+        createBusyThread();
+        bReader.readLine();
+        Object object = new Object();
+        createLockThread(object);
+    }
 
 }
