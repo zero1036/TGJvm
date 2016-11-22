e83870646d539bc233f224f884491398ab06ccbf
linzc <linzicong@ppmoney.com>
2016-11-22 08:33:21 +0800

    添加实例

---
+++ src/main/java/com/base/list/ArrayListBase.java
@@ -0,0 +1,16 @@
+package com.base.list;
+
+import java.util.*;
+
+/**
+ * Created by linzc on 2016/11/21.
+ */
+public class ArrayListBase {
+
+    public static void main(String[] args) {
+        List list = new ArrayList<String>(2);
+        list.add("a");
+        list.add("b");
+        list.add("c");
+    }
+}
