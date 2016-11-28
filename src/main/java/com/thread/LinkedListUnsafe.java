6dace8cc3d9749ae8e7ecc5c5c22022f7cda16c1
linzc <linzicong@ppmoney.com>
2016-11-28 18:41:43 +0800

    tianjia

---
+++ src/main/java/com/thread/LinkedListUnsafe.java
@@ -0,0 +1,61 @@
+package com.thread;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by linzc on 2016/11/28.
+ */
+public class LinkedListUnsafe {
+
+    private static LinkedList<Integer> list = new LinkedList<Integer>();
+
+    public static class ListAddObj implements Runnable {
+
+        private int start = 0;
+        private int end = 0;
+        private Object pLock;
+
+        public ListAddObj(int s, int e, Object lock) {
+            this.start = s;
+            this.end = e;
+            this.pLock = lock;
+        }
+
+        public void run() {
+            for (int i = start; i <= end; i++) {
+                list.addFirst(i);
+            }
+            synchronized (pLock) {
+                pLock.notifyAll();
+            }
+        }
+    }
+
+
+    /*
+    main
+     */
+    public static void main(String[] args) {
+        final Object plock = new Object();
+
+        Thread thread1 = new Thread(new ListAddObj(1, 1000, plock));
+        Thread thread2 = new Thread(new ListAddObj(1001, 2000, plock));
+        Thread thread3 = new Thread(new ListAddObj(2001, 3000, plock));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        synchronized (plock) {
+            try {
+                plock.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(list.size());
+        }
+    }
+
+}
